ccaf45609c575cc2ef88aa6bed772c5759e368e9
rita <fanrita1@gmail.com>
2022-07-25 13:49:45 +0200

    replica

--- security_groups.tf
+++ security_groups.tf
@@ -2,6 +2,42 @@ data "aws_vpc" "default_vpc_data" {
     default =true
 }
 
+resource "aws_security_group" "db_server_sg" {
+  name        = "db_server_sg"
+  vpc_id      = data.aws_vpc.default_vpc_data.id
+
+  tags = {
+    Name = "db_server_sg"
+  }
+}
+
+resource "aws_security_group_rule" "allow_db_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.db_server_sg.id
+}
+
+resource "aws_security_group_rule" "allow_mysql" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = [data.aws_vpc.default_vpc_data.cidr_block]
+  security_group_id = aws_security_group.db_server_sg.id
+}
+
+resource "aws_security_group_rule" "allow_all_db_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.db_server_sg.id
+}
+
 resource "aws_security_group" "web_server_sg" {
   name        = "web_server_sg"
   vpc_id      = data.aws_vpc.default_vpc_data.id
